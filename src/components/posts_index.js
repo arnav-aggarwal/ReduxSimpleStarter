54f1144616336d661ea9acf792b7cd741275197a
Arnav Aggarwal <arnavaggrwl@gmail.com>
2016-09-14 15:37:54 -0700

    (dev, lecture80) Get request is successful

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,5 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions/index';
 
-export default () => {
-  return <div>List of blog posts</div>
+class PostsIndex extends Component {
+  componentWillMount() {
+    console.log`this would be a good time to call an action creator to fetch posts`;
+    this.props.fetchPosts();
+  }
+
+  render() {
+    return (
+      <div>List of blog posts</div> 
+    );
+  }
 }
+
+export default connect(null, { fetchPosts })(PostsIndex);
